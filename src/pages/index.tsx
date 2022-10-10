f607a35aece123e55cea3f92c4b4c8874c1cc12a
Greg <gskril@gmail.com>
2022-10-10 09:36:47 -0500

    Abstract form submit function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { handleSubmit } from '../utils'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { Response, VotesApiResponse } from '../types'
-import { FormProps } from '../types'
 
 export default function Home() {
   const [msg, setMsg] = useState<string>('')
@@ -65,43 +64,4 @@ export default function Home() {
       </main>
     </>
   )
-}
-
-async function handleSubmit({
-  event: e,
-  setMsg,
-  setAddresses,
-  setValues,
-}: FormProps) {
-  e.preventDefault()
-  const form = e.target as HTMLFormElement
-  const address: string =
-    form.address.value || '0xc18360217d8f7ab5e7c516566761ea12ce7f9d72'
-  const startBlock: string = form['start-block'].value || '0'
-  const endBlock: string = form['end-block'].value || '20000000'
-
-  setMsg('Fetching gas costs...')
-  const res: VotesApiResponse = await fetch(
-    `/api/votes?address=${address}&start_block=${startBlock}&end_block=${endBlock}`
-  )
-    .then((res) => res.json())
-    .catch((err) => setMsg(err.message))
-
-  console.log(res.rows)
-
-  const totalSpentOnGas = res!.rows!.reduce(
-    (acc: number, curr: Response) => acc + curr.gas,
-    0
-  )
-  setMsg(
-    `${totalSpentOnGas.toFixed(2)} ETH spent on gas from ${
-      res!.rows!.length
-    } voters. Check the console for more details.`
-  )
-
-  const addresses = res!.rows!.map((row) => row.from)
-  const values = res!.rows!.map((row) => row.gas)
-
-  setAddresses(addresses)
-  setValues(values)
 }
---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+import type { FormProps, Response, VotesApiResponse } from './types'
+
+export async function handleSubmit({
+  event: e,
+  setMsg,
+  setAddresses,
+  setValues,
+}: FormProps) {
+  e.preventDefault()
+  const form = e.target as HTMLFormElement
+  const address: string =
+    form.address.value || '0xc18360217d8f7ab5e7c516566761ea12ce7f9d72'
+  const startBlock: string = form['start-block'].value || '0'
+  const endBlock: string = form['end-block'].value || '20000000'
+
+  setMsg('Fetching gas costs...')
+  const res: VotesApiResponse = await fetch(
+    `/api/votes?address=${address}&start_block=${startBlock}&end_block=${endBlock}`
+  )
+    .then((res) => res.json())
+    .catch((err) => setMsg(err.message))
+
+  console.log(res.rows)
+
+  const totalSpentOnGas = res!.rows!.reduce(
+    (acc: number, curr: Response) => acc + curr.gas,
+    0
+  )
+  setMsg(
+    `${totalSpentOnGas.toFixed(2)} ETH spent on gas from ${
+      res!.rows!.length
+    } voters. Check the console for more details.`
+  )
+
+  const addresses = res!.rows!.map((row) => row.from)
+  const values = res!.rows!.map((row) => row.gas)
+
+  setAddresses(addresses)
+  setValues(values)
+}
