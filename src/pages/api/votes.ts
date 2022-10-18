4fefba78763cb4eb339e2d927abffa0f7a5446c3
Greg <gskril@gmail.com>
2022-10-18 12:18:49 -0400

    Fix support for other contracts

--- src/pages/api/votes.ts
+++ src/pages/api/votes.ts
@@ -6,13 +6,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<VotesApiResponse>
 ) {
-  const { address, start_block, end_block, type } = req.query
+  const { address: _address, start_block, end_block, type } = req.query
+  const address = _address!.toString().toLowerCase()
+
+  const ensContracts = [
+    '0xc18360217d8f7ab5e7c516566761ea12ce7f9d72',
+    '0x323a76393544d5ecca80cd6ef2a560c6a395b7e3',
+  ]
+
   const sortQuery = `
     select
       "from",
       t.gas.transaction_fee.eth as gas
     from
-      user14.transaction t
+      ${
+        ensContracts.includes(address)
+          ? 'user14.transaction'
+          : 'ethereum.transaction'
+      } t
     where
         "to" = '${address}'
         and t.function.name like '${type}'
