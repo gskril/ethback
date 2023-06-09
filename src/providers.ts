728cf4134b42f7c12f46f7bb239c0f2cbe6877c9
Greg <gskril@gmail.com>
2023-06-08 23:17:01 -0400

    Add spot for RPC

--- src/providers.ts
+++ src/providers.ts
@@ -1,16 +1,19 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
+const ALCHEMY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || ''
 export const chains = [mainnet, goerli]
 
 const { publicClient, webSocketPublicClient } = configureChains(chains, [
+  alchemyProvider({ apiKey: ALCHEMY }),
   publicProvider(),
 ])
 
 const { connectors } = getDefaultWallets({
-  appName: 'ethback.xyz',
+  appName: 'Web3 Starter',
   chains,
 })
 
